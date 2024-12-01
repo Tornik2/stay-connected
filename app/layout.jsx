61e88b4227417dbb2409b8442aa443fa04d0847e
Tornik2 <tornik2@yahoo.com>
2024-12-01 18:44:10 +0400

    add Header Components to RootLayout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,12 @@
-import HomePage from "./page"
-
-export default function RootLayout({children}) {
- return(
+import HomePage from "./page";
+import Header from "./components/Header/Header";
+export default function RootLayout({ children }) {
+  return (
     <html>
- <body>
-    {children}
- </body>
- </html>)
-  }
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
