a801cd2d07b62caad80cbe7ec247421581d2ebeb
Tornike Sharvashidze <tornik2@yahoo.com>
2024-12-02 18:54:23 +0100

    fix bug for retrieving accesstoken in middleware

--- app/middleware.js
+++ app/middleware.js
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export function middleware(request) {
   const cookieStore = cookies();
-  const accessToken = cookieStore.get("accessToken");
+  const accessToken = cookieStore.get("accessToken")?.value;
   // If no access token, redirect to the login page
   if (!accessToken) {
     return NextResponse.redirect(new URL("/login", request.url));
