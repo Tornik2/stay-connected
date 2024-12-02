d8983d06744b4c47f735dd45535ec908cce0fb87
Tornike Sharvashidze <tornik2@yahoo.com>
2024-12-02 19:49:39 +0100

    add logout API route
    deleting cookie on server side

---
+++ app/api/logout/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function GET() {
+  const cookieStore = await cookies();
+  cookieStore.delete("accessToken");
+  cookieStore.delete("refreshToken");
+
+  return NextResponse.redirect(new URL("/login", request.url));
+}
